e65a9d6d697b5aab0117df69e6f1de04be665e28
amir shahedi <shahediamirmahdi@gmail.com>
2023-08-15 23:38:39 +0330

    about page complated, called from python

--- code/app.py
+++ code/app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
