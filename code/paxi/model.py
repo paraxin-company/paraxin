9c2b50f022fbc953f0526473446694fb52e7d7e7
amir shahedi <shahediamirmahdi@gmail.com>
2023-12-07 15:15:54 +0330

    create base verify template for verify phone and email

--- code/paxi/model.py
+++ code/paxi/model.py
@@ -12,6 +12,9 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(40))
     username = db.Column(db.String(40), unique=True, nullable=False)
+    phone = db.Column(db.String(11), nullable=False)
+    # verify value use for email and phone varification, first number is for email and second is for phone
+    verify = db.Column(db.String(2), nullable=False, default='00')
     email = db.Column(db.String(40), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
     profile = db.Column(db.Text, nullable=False)
